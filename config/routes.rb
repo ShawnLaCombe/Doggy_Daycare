ed5848f480b7526261f1ade7966d4e3c110c7f30
Shawn <lacombeshawn@yahoo.com>
2016-08-02 11:02:39 -0500

    Add index and about view/controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/about'
+
   resources :dogs
   resources :vets
   resources :owners
+
+  root 'pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
